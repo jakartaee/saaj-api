57abec752c1211bf42ffff7706cdf51f05fed9b3
Tomas Kraus <Tomas.Kraus@oracle.com>
2019-07-17 14:19:58 +0200

    Spotbugs evaluation turned on.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- etc/jenkins/continuous.groovy
+++ etc/jenkins/continuous.groovy
@@ -70,6 +70,7 @@ pipeline {
                         '''
                     }
                 }
+                recordIssues(tools: [spotBugs(useRankAsPriority: true)])
             }
         }
       
