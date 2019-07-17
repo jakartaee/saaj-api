0aaa4394d67eeb624a3b29878bbab74c7ae5eeb6
Tomáš Kraus <tomas.kraus@oracle.com>
2019-07-17 15:31:23 +0200

    Added continuous build and fixed release job (#36)
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- etc/jenkins/continuous.groovy
+++ etc/jenkins/continuous.groovy
@@ -61,7 +61,7 @@ pipeline {
             }
         }
         // Perform release
-        stage('Release') {
+        stage('Build') {
             steps {
                 configFileProvider([
                         configFile(
@@ -73,16 +73,14 @@ pipeline {
                             targetLocation: '/home/jenkins/.m2/'
                         )]) {
                     sshagent([SSH_CREDENTIALS_ID]) {
-                        sh '''
-                            etc/jenkins/release.sh "${SPEC_VERSION}" "${NEXT_SPEC_VERSION}" \
-                                                   "${API_VERSION}" "${NEXT_API_VERSION}" \
-                                                   "${DRY_RUN}" "${OVERWRITE}"
-                        '''
+                        sh 'etc/jenkins/continuous.sh'
                     }
                 }
+                junit '**/target/surefire-reports/*.xml'
+                recordIssues(tools: [spotBugs(useRankAsPriority: true)])
             }
         }
-      
+        
     }
 
 }
