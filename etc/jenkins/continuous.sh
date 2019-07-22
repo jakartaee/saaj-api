40857230db0c3416eb15fcab2203b3a7f915c5dd
Tomas Kraus <Tomas.Kraus@oracle.com>
2019-07-22 11:39:22 +0200

    Modified spec build arguments.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- etc/jenkins/continuous.sh
+++ etc/jenkins/continuous.sh
@@ -12,6 +12,6 @@
 #  N/A
 
 echo '-[ Jakarta SOAP with Attachments Specification Build ]--------------------------'
-(cd spec && mvn -U -C -Psnapshots,oss-release clean install deploy)
+(cd spec && mvn -U -C -Psnapshots,oss-release -Dstatus='DRAFT' clean install deploy)
 echo '-[ Jakarta SOAP with Attachments API Build ]------------------------------------'
 (cd api && mvn -U -C -Psnapshots,oss-release clean install spotbugs:spotbugs deploy)
